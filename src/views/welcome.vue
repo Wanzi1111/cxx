ce67a1400a04c850240d332093e01b2d3ba31ab5
xuxiaojie <xu15201130897@qq.com>
2023-04-20 19:23:10 +0800

    feat: 新增

---
+++ src/views/welcome.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="back">
+    <p class="title">欢迎来到陈星旭专场</p>
+   </div>
+</template>
+<script>
+export default {
+   name: '',
+   data() {
+       return {}
+   },
+}
+</script>
+<style lang='less' scoped>
+@import '../common/style/mixin';
+.back{
+    width: 100%;
+    height: 100%;
+    background: @primary;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .title{
+        font-size: 20px;
+        color: #fff;
+        font-weight: bold;
+    }
+}
+</style>
